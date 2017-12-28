a727dafec0f7643db59215670466e9098db834d3
moon <mgj90@naver.com>
2017-12-28 10:40:41 +0900

    age 게터 세터 추가

--- test/src/test/PersonVO.java
+++ test/src/test/PersonVO.java
@@ -8,6 +8,12 @@ public class PersonVO {
 	private int age;
 	
 	
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
 	public String getName() {
 		return name;
 	}
