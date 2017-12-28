604ea3954230abb4bc1ba47841d906b6c4067b1a
moon <mgj90@naver.com>
2017-12-28 10:52:45 +0900

    c

--- test/src/test/PersonVO.java
+++ test/src/test/PersonVO.java
@@ -9,6 +9,12 @@ public class PersonVO {
 	private String gender;
 	
 	
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
 	public int getAge() {
 		return age;
 	}
